276872ddc3971fbdfbfd9bd9193cdc9654f53ad5
ricqosu <quachr@oregonstate.edu>
2023-11-13 21:25:04 -0800

    Implement email validator

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3005;
+
+app.use(express.json());
+
+app.post("/validate-email", (req, res) => {
+  const email = req.body.email;
+
+  // Email Pattern
+  // Can accept upper & lower case letters, numbers, periods, underscores, dashes
+  // Followed by an '@' then domain name, then the TLD (.com, .net, etc)
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  // Returns true if valid email, false otherwise
+  res.json({ isValid: emailRegex.test(email) });
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`);
+});
